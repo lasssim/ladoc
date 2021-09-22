3f07c8747aee82e80e904e582274605f341daa36
Simon Lasselsberger <simon@lasssim.com>
2021-09-22 13:29:13 +0200

    extend footer etc.

--- mkdocs/mkdocs_macros.py
+++ mkdocs/mkdocs_macros.py
@@ -9,4 +9,4 @@ def define_env(env):
    # create a jinja2 filter
     @env.macro
     def api_spec_link(yaml_filenamepath, label="API"):
-      return "[" + label + "](/doc/assets/mkdocs/redoc.html?url=/" + yaml_filenamepath + ")"
+      return "[" + label + "](/redoc.html?url=/" + yaml_filenamepath + ")"
