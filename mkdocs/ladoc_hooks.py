a3dbb17e7a9f48e9e9413d7e5cc058238ece2844
Simon Lasselsberger <simon@lasssim.com>
2022-04-15 11:01:54 +0200

    Fix hidden files hook

--- mkdocs/ladoc_hooks.py
+++ mkdocs/ladoc_hooks.py
@@ -11,14 +11,18 @@ def add_hidden_files(files, config):
   print("from on_pre_build")
   pp = pprint.PrettyPrinter(indent=4)
   hidden_files_globs = [
-    ".ladoc/api_helpers/*",
-    ".ladoc/assets/*"
+    ".ladoc/api_helpers/**",
+    ".ladoc/assets/**"
   ]
   for hidden_files_glob in hidden_files_globs:
     absolute_hodden_files_glob = os.path.join(config['docs_dir'], hidden_files_glob)
     hidden_files = glob.glob(absolute_hodden_files_glob, recursive=True)
+    pp.pprint(hidden_files)
     for hidden_filename in hidden_files:
+      if(os.path.isdir(hidden_filename)):
+        continue
       file = File(hidden_filename.replace(config['docs_dir']+"/", ''), config['docs_dir'], config['site_dir'], config['use_directory_urls'])
+      pp.pprint(file)
       files.append(file)
 
   return files
