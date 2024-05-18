7ccd0b568185ce65879dd76aebb7bae2e67c77b1
Simon Lasselsberger <simon@lasssim.com>
2024-05-18 16:31:14 +0200

    add macro

--- mkdocs/mkdocs_macros.py
+++ mkdocs/mkdocs_macros.py
@@ -1,3 +1,5 @@
+import os
+
 def define_env(env):
     """
     This is the hook for defining variables, macros and filters
@@ -10,3 +12,17 @@ def define_env(env):
     @env.macro
     def api_spec_link(yaml_filenamepath, label="API"):
       return "[" + label + "](/redoc.html?url=/" + yaml_filenamepath + ")"
+    
+    @env.macro
+    def generate_subnav(current_page):
+        subnav = []
+        path = os.path.dirname(current_page.file.src_path)
+        for root, dirs, files in os.walk(path):
+            level = root.replace(path, '').count(os.sep)
+            indent = ' ' * 4 * (level)
+            subnav.append(f"{indent}- [{os.path.basename(root)}]({root}/index.md)")
+            sub_indent = ' ' * 4 * (level + 1)
+            for f in files:
+                if f.endswith('.md') and f != 'index.md':
+                    subnav.append(f"{sub_indent}- [{f[:-3]}]({root}/{f})")
+        return '\n'.join(subnav)   
