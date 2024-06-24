4276854a8bc124122ea6275e49370443115bd231
Simon Lasselsberger <simon@lasssim.com>
2024-06-24 11:15:04 +0200

    Add git url manipulation

--- mkdocs/build-entrypoint.sh
+++ mkdocs/build-entrypoint.sh
@@ -14,7 +14,12 @@ echo "  - DEPLOY_BRANCH: $DEPLOY_BRANCH"
 echo "  - VERSION_NAME: $VERSION_NAME"
 echo "  - GIT_COMMITTER_NAME: $GIT_COMMITTER_NAME"
 echo "  - GIT_COMMITTER_EMAIL: $GIT_COMMITTER_EMAIL"
-echo
+echo "  - GIT_URL_FROM: $GIT_URL_FROM"
+echo "  - GIT_URL_TO: $GIT_URL_TO"
+
+echo "  Changing git url to use CI_JOB_TOKEN..."
+# Change the git url to use the CI_JOB_TOKEN
+git config --global url."$GIT_URL_TO".insteadOf "$GIT_URL_FROM" 
 
 echo "  Setting default branch to $DEFAULT_BRANCH..." 
 mike set-default $DEFAULT_BRANCH --allow-undefined --branch $DEPLOY_BRANCH
