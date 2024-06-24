0e3321b26ea34891878eee7dec6fc9ba44e84af2
Simon Lasselsberger <simon@lasssim.com>
2024-06-24 10:31:41 +0200

    Add deploy branch

--- mkdocs/build-entrypoint.sh
+++ mkdocs/build-entrypoint.sh
@@ -10,16 +10,17 @@ echo
 echo "  - Current directory: $(pwd)"
 echo "  - DEFAULT_BRANCH: $DEFAULT_BRANCH"
 echo "  - CURRENT_BRANCH: $CURRENT_BRANCH"
+echo "  - DEPLOY_BRANCH: $DEPLOY_BRANCH"
 echo "  - VERSION_NAME: $VERSION_NAME"
 echo "  - GIT_COMMITTER_NAME: $GIT_COMMITTER_NAME"
 echo "  - GIT_COMMITTER_EMAIL: $GIT_COMMITTER_EMAIL"
 echo
 
 echo "  Setting default branch to $DEFAULT_BRANCH..." 
-mike set-default $DEFAULT_BRANCH --allow-undefined
+mike set-default $DEFAULT_BRANCH --allow-undefined --branch $DEPLOY_BRANCH
 
 echo "  Building site..."
 # Build the site
-mike deploy $VERSION_NAME --push
+mike deploy $VERSION_NAME --push --branch $DEPLOY_BRANCH
 
 echo "  done."
