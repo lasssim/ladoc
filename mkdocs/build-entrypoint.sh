ebc7f7ff3a1227c9201568007f0744b3d7b98b31
Simon Lasselsberger <simon@lasssim.com>
2024-06-24 10:25:10 +0200

    Fix build script

--- mkdocs/build-entrypoint.sh
+++ mkdocs/build-entrypoint.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
-mike set-default $DEFAULT_BRANCH --allow-undefined
+set -e
 
-# Build the site
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 VERSION_NAME=$(echo $CURRENT_BRANCH | tr '/' '-')
+
+echo "Building site..."
+echo
+echo "  - Current directory: $(pwd)"
+echo "  - DEFAULT_BRANCH: $DEFAULT_BRANCH"
+echo "  - CURRENT_BRANCH: $CURRENT_BRANCH"
+echo "  - VERSION_NAME: $VERSION_NAME"
+echo "  - GIT_COMMITTER_NAME: $GIT_COMMITTER_NAME"
+echo "  - GIT_COMMITTER_EMAIL: $GIT_COMMITTER_EMAIL"
+echo
+
+echo "  Setting default branch to $DEFAULT_BRANCH..." 
+mike set-default $DEFAULT_BRANCH --allow-undefined
+
+echo "  Building site..."
+# Build the site
 mike deploy $VERSION_NAME --push
 
+echo "  done."
